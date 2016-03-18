59e66d8f69e548b23c22524d99ec80c868674b6a
CDDHK <394300338@qq.com>
2016-03-19 02:30:10 +0800

    update 05

--- src/com/atguigu/github/test/HelloGithub.java
+++ src/com/atguigu/github/test/HelloGithub.java
@@ -8,6 +8,7 @@ public class HelloGithub {
 		System.out.println("github 你好2");
 		System.out.println("github 你好3");
 		System.out.println("github 你好4");
+		System.out.println("github 你好5");
 		
 	}
 	
